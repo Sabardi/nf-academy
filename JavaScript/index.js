8481830cbcd69228370401bedc172197fa5e76d4
Sabardi <Sabardibahari@gmail.com>
2024-03-08 10:17:03 +0800

    add belajar js

---
+++ JavaScript/index.js
@@ -0,0 +1,12 @@
+var nama = "sabardi"
+var pekerjaan = "petani"
+let umur = 20
+let berat = 45.5
+
+console.log(nama, pekerjaan,"dan umur "+ umur, berat)
+
+let a = 10
+let b = 10
+hasil = a + b
+
+console.log(hasil)
---
+++ JavaScript/index2.js
@@ -0,0 +1,7 @@
+let jari2 = 15;
+let phi = 3.14; //const
+let luas = phi * jari2 **2;
+let keliling = 2 * phi * jari2;
+
+console.log(luas )
+console.log(keliling )
