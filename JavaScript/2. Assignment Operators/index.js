6fe8d7b0b87a970b0aa9cf107cb8cd5a101f79eb
Sabardi <Sabardibahari@gmail.com>
2024-03-09 22:50:42 +0800

    penambahan materi assigment

---
+++ JavaScript/2. Assignment Operators/index.js
@@ -0,0 +1,17 @@
+// Assignment Operators
+// Assignment operators assign values to JavaScript variables.
+// atau opertor penugasan yang menetapkan value/nilai pada variable itu
+
+let nilai1 = 20
+// let nilai2 = nilai1 += 10
+// let nilai3 = nilai1 -= 10
+// let nilai4 = nilai1 *= 10
+let nilai5 = nilai1 /= 10
+
+// console.log("nilai nya =" + nilai1)
+// console.log("nilai nya +=" + nilai2)
+// console.log("nilai nya -=" + nilai3)
+// console.log("nilai nya *=" + nilai4)
+console.log("nilai nya /=" + nilai5)
+
+// untuk penggunaan assigment ini adalah harus di komen dl 🤣🤣🤣🤣
