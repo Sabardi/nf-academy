bd21962151de233141c26136a85ad99f1e7e8a10
Sabardi <Sabardibahari@gmail.com>
2024-03-09 22:33:07 +0800

    operator aritmatika

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -5,8 +5,30 @@ let berat = 45.5
 
 console.log(nama, pekerjaan,"dan umur "+ umur, berat)
 
+// aritmatika
+/** + untuk opeasi penjumlahan.
+- untuk operasi pengurangan.
+* untuk operasi perkalian.
+/ untuk operasi pembagian.
+% untuk operasi sisa pembagian.
+++ untuk operasi penambahan angka satu dari bilangan sebelumnya.
+-- untuk operasi pengurangan angka satu dari bilangan sebelumnya.
+ */
+
 let a = 10
 let b = 10
-hasil = a + b
+penjumlahan = a + b
+pengurangan = a - b
+perkalian = a * b
+pembagian = a / b
+modulus = a % b
+increment = ++a
+decrement = --b
 
-console.log(hasil)
+console.log("Hasil Penjumlahan ="+penjumlahan)
+console.log("Hasil Pengurangan ="+pengurangan)
+console.log("Hasil perkalian ="+perkalian)
+console.log("Hasil pembagian ="+pembagian)
+console.log("Hasil modulus ="+modulus)
+console.log("Hasil increment ="+ increment)
+console.log("Hasil decrement ="+ decrement)
