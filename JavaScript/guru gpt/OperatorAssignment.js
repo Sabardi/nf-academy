d3e889fddc296d4277c9d48293c4dbb843010630
Sabardi <Sabardibahari@gmail.com>
2024-03-10 15:12:30 +0800

    operator assigment

---
+++ JavaScript/guru gpt/OperatorAssignment.js
@@ -0,0 +1,21 @@
+// operator assignment adalah tanda sama dengan (=). Ini digunakan untuk memberikan nilai kepada variabel. 
+let x = 10
+x+= 20 //x nya di tambah dengan nilai 20 dan otomatis akan di jumlahkan
+console.log("assignment penjumlahan = " + x)
+
+let a = 2
+a-=1
+console.log("pengurangan = " + a)
+
+let b = 2
+b*= 3
+console.log("perkalian = "+b)
+
+let c = 5
+c%=3
+console.log("modulus = "+c)
+
+// penggabungan string dan penugasan
+let nama = "sabardi"
+nama+=" baharudin"
+console.log("gabungan string = "+nama)
