d1ba3f8cc9b5c6b1ca2ce0c6fe35d091e47c5de2
Sabardi <Sabardibahari@gmail.com>
2024-03-10 15:00:03 +0800

    operator aritmatika

---
+++ JavaScript/guru gpt/Operator.js
@@ -0,0 +1,25 @@
+let hasil1 = 5 + 3; // hasilnya 8
+console.log("hasil nya = " + hasil1)
+
+let hasil2 = 5 - 3; // hasilnya 2
+console.log( "hasil nya = " +  hasil2)
+
+let hasil3 = 5 * 3; // hasilnya 15
+console.log( "hasil nya = " + hasil3)
+
+let hasil4 = 15 / 3; // hasilnya 5
+console.log( "hasil nya = " + hasil4)
+
+let hasil5 = 5 % 3; // hasilnya 2
+console.log( "hasil nya = " + hasil5)
+
+let angka1 = 5;
+angka1++; // sekarang angka adalah 6
+console.log("++ = "+angka1)
+
+let angka2 = 5;
+angka2--; // sekarang angka adalah 4
+console.log("hasil nya = " + angka2)
+
+let hasil = 5 ** 3; // hasilnya 125
+console.log("hasil = " + hasil)
