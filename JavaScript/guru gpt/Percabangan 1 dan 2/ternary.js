7af828298ff4380373bb3a7b038f3936614702c6
Sabardi <Sabardibahari@gmail.com>
2024-03-12 08:37:51 +0800

    penambahan ternary

---
+++ JavaScript/guru gpt/Percabangan 1 dan 2/ternary.js
@@ -0,0 +1,6 @@
+let nilai = 60;
+let hasil = (nilai >=80)? "Selamat anda lulus dengan nilai A": 
+// (nilai >=75)? "selamat anda mendapatkan nilai B":
+// (nilai >=60)? "selamat anda mendapatkan nilai C":
+"maaf anda belom lulus";
+console.log(hasil)
