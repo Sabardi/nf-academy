0d3f32b8e83c932901d689b5229e129068060b88
Sabardi <Sabardibahari@gmail.com>
2024-03-12 10:41:52 +0800

    perulangan bersarang

--- JavaScript/guru gpt/perulangan/foreach.js
+++ JavaScript/guru gpt/perulangan/foreach.js
@@ -5,3 +5,10 @@ angka.forEach(function (index) {
 
 angka.forEach(index =>console.log("iterasi nya - " +index))
 
+
+for (let i = 1; i <= 3; i++) {
+    for (let j = 1; j <= 3; j++) {
+        console.log(`Nilai i: ${i}, Nilai j: ${j}`);
+    }
+}
+
