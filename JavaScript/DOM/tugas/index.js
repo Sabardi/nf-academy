66df0722eae68568988ca855ea7f88cc60ae1f59
Sabardi <Sabardibahari@gmail.com>
2024-03-19 22:08:54 +0700

    bisa menampilkan  hasil

--- JavaScript/DOM/tugas/index.js
+++ JavaScript/DOM/tugas/index.js
@@ -29,13 +29,13 @@ function hitung(){
 
     }else if(jabatan == "Asisten Manager"){
         gaji_pokok = 10000000
-        tunjangan = 0.15 * gaji
+        tunjangan = 0.15 * gaji_pokok
         bpjs = 0.1 * gaji_pokok
         gajibersih = (gaji_pokok + tunjangan) - bpjs
 
     }else if(jabatan == "Staff"){
         gaji_pokok = 5000000
-        tunjangan = 0.15 * gaji
+        tunjangan = 0.15 * gaji_pokok
         bpjs = 0.1 * gaji_pokok
         gajibersih = (gaji_pokok + tunjangan) - bpjs
 
@@ -45,9 +45,57 @@ function hitung(){
 keluarga = (status == "Sudah Menikah") ? 0.2 * gaji_pokok:0
 let totalGaji= gajibersih + keluarga
     // let potongBpjs = totalGaji - bpjs
+// hasil
+let hasil = document.getElementById("hasil");
+hasil.innerHTML = `
+<table border="1" style="border-collapse: collapse; width: 100%;">
+<thead>
+    <tr>
+        <th colspan="2">Cetak Data Pegawai</th> 
+    </tr>
+</thead>
+<tbody>
+    <tr>
+        <td>Nama</td>
+        <td>${nama}</td>
+    </tr>
+    <tr>
+        <td>Jabatan</td>
+        <td>${jabatan}</td>
+    </tr>
+    <tr>
+        <td>Status</td>
+        <td>${status}</td>
+    </tr>
+    <tr>
+        <td>Gaji Pokok</td>
+        <td>${gaji_pokok}</td>
+    </tr>
+    <tr>
+        <td>Tunjangan</td>
+        <td>${tunjangan}</td>
+    </tr>
+    <tr>
+        <td>Nama</td>
+        <td>${bpjs}</td>
+    </tr>
+    <tr>
+        <td>Tunjangan keluarga</td>
+        <td>${keluarga}</td>
+    </tr>
+
+</tbody>
+<tfoot>
+    <tr>
+        <td>Gaji bersih</td>
+        <td>${totalGaji}</td>
+    </tr>
+</tfoot>
+`
+
 
 swal(`
-    Nama Pelanggan : ${nama}
+    Nama  : ${nama}
     Jabatan : ${jabatan}
     status  : ${status}
     gaji pokok : ${gaji_pokok}
@@ -57,3 +105,5 @@ swal(`
     gaji bersih : ${totalGaji}
     `);
 }
+
+
