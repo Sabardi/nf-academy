fc2b1db09961d3b0ea09d41776032ff7d9a4c07d
Sabardi <Sabardibahari@gmail.com>
2024-03-26 16:29:02 +0700

    Kumpulkan Tugas

---
+++ PHP/codePhp.php
@@ -0,0 +1,31 @@
+<?php
+// data diri
+$foto = "sabardi.JPG";
+$nama = "sabardi";
+$unive = "Universitas Qamarul Huda Badarudin";
+$prodi = "Teknologi Informasi";
+$skill = "Desain Web";
+$skil2 = "Ms Office";
+$universitas = "Saya mahasiswa $unive sedang menempuh pendidikan di PT.Nurul Fikri Cipta inovasi";
+$tentang = "Hii 👋👋👋👋👋 salama kenal. saya .$nama Dari .$unive saya dari prodi $prodi senang berkenalan dengan anda!. saya sedang belajar web full stack dan saya sering mengikuti banyak kegiatan di luar dan sekarang saya belajar 
+di PT. NURUL FIKRI CIPTA INOVASI. Tidak Hanya itu saya sering mengikuti kegiatan offline. dan kekurangan saya masih belom menerapkan ilmu yang saya dapatkan " ;
+
+$TTL = "apitaik, 04, Januri 2000";
+$website = "https://github.com/Sabardi";
+$noHp = "087863968484";
+$alamat = "apitaik Lombok Timur";
+$umur = 30;
+$status = "Mahasiswa";
+$email = "sabardibahari@gmail.com";
+$pekerjaan = "Belom Bekerja";
+
+$moto = "cepat atau lambat kamu dalam belajar Rasa capek yang kamu dapatkan sama, Belajar di 20 jam pertama menentukan kebiasaan mu";
+//sosial media
+$facebook = "https://www.facebook.com/sabardi.sihantulaut/";
+$instagram = "https://www.instagram.com/sabardi_bajang_bahari/";
+$linkedin = "https://www.linkedin.com/in/sabardi-baharudin-375045236/";
+$twitter = "https://twitter.com/Sabardi_";
+
+
+
+?>
