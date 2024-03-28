cddf1f560f66cb30cc064b2e139034ee3d3a34b4
Sabardi <Sabardibahari@gmail.com>
2024-03-28 09:50:28 +0700

    pemindahan file dan penambahan mayteri ke 3

---
+++ php/pertemuan-3/index.php
@@ -0,0 +1,11 @@
+<?php
+for ($x=1; $x <= 10 ; $x+=1) { 
+    # code...
+    echo "dimulai dari $x <br>";
+}
+echo "<hr>";
+for ($y= 10; $y >= 1 ; $y--) { 
+    # code...
+    echo "dimulai dari $y <br>";
+}
+?>
