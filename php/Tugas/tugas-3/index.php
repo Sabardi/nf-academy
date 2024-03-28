4fe62c05b98ba5b028cc923ba7b5552bd788ff3c
Sabardi <Sabardibahari@gmail.com>
2024-03-28 21:31:22 +0700

    finish kumpulkan tugas finish

--- php/Tugas/tugas-3/index.php
+++ php/Tugas/tugas-3/index.php
@@ -167,8 +167,8 @@ $keterangan = [
     </div>
 
     <footer class="footer">
-        <div class="container text-center bg-primary">
-            <span class="text-muted">Copyright &copy Sabardi baharudin</span>
+        <div class="container text-center">
+            <span class="text-bold">Copyright &copy 2024 Sabardi | Everyting gonna be oky </span>
         </div>
     </footer>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
