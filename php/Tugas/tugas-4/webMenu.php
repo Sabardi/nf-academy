d02bdf5c4655281c07eb855a07b5e93e107f3eba
Sabardi <Sabardibahari@gmail.com>
2024-04-01 15:29:19 +0700

    try use templet

---
+++ php/Tugas/tugas-4/webMenu.php
@@ -0,0 +1,22 @@
+<?php
+// menggunakan  associative array
+
+//menu untuk bagian atas
+$menuAtas = [
+    "Home"=>"Home",
+    "Shop"=>"Shop", 
+    "Pages"=>"Pages",
+    "Blog"=>"Blog",
+    "Contacts"=>"Contacts"
+];
+
+// menu untuk di bagian bawah 
+$menuBawah = [
+    "Home"=>"Home.php",
+    "Shop"=>"Shop.php", 
+    "Pages"=>"Pages.php",
+    "Blog"=>"Blog.php",
+    "Contacts"=>"Contacts"
+
+];
+?>
