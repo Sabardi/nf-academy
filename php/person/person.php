2084f528cfd5d43ad933dc723d9bf9e5dfbfaab2
Sabardi <Sabardibahari@gmail.com>
2024-04-03 10:01:56 +0700

    istirahat

---
+++ php/person/person.php
@@ -0,0 +1,17 @@
+<?php
+
+class person{
+    protected $nama;
+    protected $gender;
+    public function __construct($nama, $gender){
+        $this->nama = $nama;
+        $this->gender = $gender;
+    }
+
+    public function cetak(){
+        echo "<br>Nama: ".$this->nama;
+        echo "<br>Gender: ".$this->gender;
+}
+}
+
+?>
